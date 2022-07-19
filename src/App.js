4dd92371bd91e19d147b0bfd4f0f5324448f62d0
orhalimi <or50002001@gmail.com>
2022-07-19 18:42:00 +0300

    baseline to run

--- src/App.js
+++ src/App.js
@@ -1,75 +1,77 @@
-import socket from "./utils/socketIO";
-import { useState, useEffect } from "react";
+import socket from './utils/socketIO';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [user, setUser] = useState({
-    sessionID: localStorage.getItem("sessionID") || "",
-    userId: "",
-    username: "",
-  });
-  const [isConnected, setIsConnected] = useState(socket.connected);
-
-  function connectSocket() {
-    if (!socket.connected) {
-      socket.auth = { ...user };
-      socket.connect();
-    } else {
-      console.log(socket.connected);
-    }
-  }
-
-  useEffect(() => {
-    socket.on("connect", () => {
-      setIsConnected(true);
+    const [user, setUser] = useState({
+        sessionID: localStorage.getItem('sessionID') || '',
+        userId: '',
+        username: '',
     });
-    socket.on("disconnect", () => {
-      setIsConnected(false);
-    });
+    const [isConnected, setIsConnected] = useState(socket.connected);
 
-    socket.on("session", (userSession) => {
-      setUser(userSession);
-      localStorage.setItem("sessionID", userSession.sessionID);
-    });
-
-    if (user.sessionID) {
-      console.log("fff");
-      connectSocket();
+    function connectSocket() {
+        if (!socket.connected) {
+            socket.auth = { ...user };
+            socket.connect();
+        } else {
+            console.log(socket.connected);
+        }
     }
 
-    return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-    };
-  }, []);
+    useEffect(() => {
+        socket.on('connect', () => {
+            setIsConnected(true);
+        });
+        socket.on('disconnect', () => {
+            setIsConnected(false);
+        });
 
-  return (
-    <div className="App">
-      {!isConnected ? (
-        <>
-          <input
-            type="text"
-            placeholder="Insert username"
-            value={user.username}
-            onChange={({ target }) =>
-              setUser((u) => ({
-                ...u,
-                username: target.value,
-              }))
-            }
-          />
-          <button
-            style={{ marginLeft: "15px", border: "2px solid black" }}
-            disabled={!user.username}
-            onClick={connectSocket}
-          >
-            Connect!
-          </button>
-        </>
-      ) : (
-        <div>You are connected as {JSON.stringify(user)}!</div>
-      )}
-    </div>
-  );
+        socket.on('session', (userSession) => {
+            setUser(userSession);
+            localStorage.setItem('sessionID', userSession.sessionID);
+        });
+
+        if (user.sessionID) {
+            connectSocket();
+        }
+
+        return () => {
+            socket.off('connect');
+            socket.off('disconnect');
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            {!isConnected ? (
+                <>
+                    <input
+                        type="text"
+                        placeholder="Insert username"
+                        value={user.username}
+                        onChange={({ target }) =>
+                            setUser((u) => ({
+                                ...u,
+                                username: target.value,
+                            }))
+                        }
+                    />
+                    <button
+                        style={{
+                            marginLeft: '15px',
+                            border: '2px solid black',
+                        }}
+                        disabled={!user.username}
+                        onClick={connectSocket}
+                    >
+                        Connect!
+                    </button>
+                </>
+            ) : (
+                <div>You are connected as {JSON.stringify(user)}!</div>
+            )}
+        </div>
+    );
 }
 
 export default App;
